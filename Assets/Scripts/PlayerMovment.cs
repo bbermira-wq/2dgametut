cc539e10b9c97a501e6e5b915409b42c6ece4124
bbermira-wq <bbermira@gmail.com>
2025-10-06 16:46:17 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovment.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerMovment : MonoBehaviour
+{
+    private Rigidbody2D body; 
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D> ();
+    }
+
+}
