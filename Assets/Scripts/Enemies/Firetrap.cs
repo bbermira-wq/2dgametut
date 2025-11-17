4727d738ec40727c8ac5e921d0395bc701e6f8fe
bbermira-wq <bbermira@gmail.com>
2025-11-17 16:38:45 +0100

    17/11/25
    Dodadovme traps(spikes, fire damage, spikehead)

---
+++ Assets/Scripts/Enemies/Firetrap.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firetrap : MonoBehaviour
+{
+    [SerializeField] private float damage;
+
+    [Header("Firetrap Timers")]
+    [SerializeField] private float activationDelay;
+    [SerializeField] private float activeTime;
+
+    private Animator anim;
+    private SpriteRenderer spriteRend;
+
+    private bool triggered; //when the trap gets triggered;
+    private bool active; // when the trap is active and can hurt the player
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            if (!triggered)
+                StartCoroutine(ActivateFiretrap());
+    
+            if (active)
+                collision.GetComponent<Health>().TakeDamage(damage);
+        }
+    }
+
+    private IEnumerator ActivateFiretrap()
+    {
+        // turn the sprite red to notify the player and trigger the trap
+        triggered = true;
+        spriteRend.color = Color.red;  
+
+        // wait for delay, activate trap, turn on animation, return color back to normal
+        yield return new WaitForSeconds(activationDelay);
+        spriteRend.color = Color.white; // turn the sprite white to notify the player
+        active = true;
+        anim.SetBool("activated", true);
+
+        //wait for X seconds, deactive traps and reset all variables and animator
+        yield return new WaitForSeconds(activeTime);
+        active = false;
+        triggered = false;
+        anim.SetBool("activated", false);
+
+    }
+}
---
+++ Assets/Scripts/Enemies/Spikehead.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Spikehead : EnemyDamage
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float range;
+    [SerializeField] private float checkDelay;
+    private float checkTimer;
+    private Vector3 destination;
+
+    private bool attackting;
+
+    private void Update()
+    {
+        //move spikes only to destination if attacking
+        if(attackting)
+            transform.Translate(destination * Time.deltaTime * speed);
+        else
+        {
+            checkTimer += Time.deltaTime;
+            if (checkTimer > checkDelay)
+                CheckForPlayer();
+        }
+    }
+}
